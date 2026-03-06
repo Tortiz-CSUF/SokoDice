bfe7fd07ac2adac8fea0411b45dafebfb7cd160f
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-06 23:19:44 +0000

    Main Menu and Level Select
    Finished Main Menu Script and start level selection scene.

---
+++ main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@export var Level_Select_Path: String = "res://Level_Select.tscn"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Intentionally passed
+
+
+
+func _on_start_button_pressed() -> void:
+	# Loads in level select scene
+	# MUST SET LEVEL SELECT SCENE ONCE CREATE
+	get_tree().change_scene_to_file(Level_Select_Path)
+
+
+func _on_exit_button_pressed() -> void:
+	# Quits application 
+	get_tree().quit()
