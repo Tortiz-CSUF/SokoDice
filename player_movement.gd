c0a40e7d53b6b499320a78da6ca2045d8195b903
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-08 02:28:57 +0000

    player_movement script
    - create player movement script

--- player_movement.gd
+++ player_movement.gd
@@ -2,15 +2,73 @@ extends Node2D
 
 # Animations
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var tilemap: TileMapLayer = get_parent().get_node("TileMapLayer")
+
+@export var tile_size: int = 32
+@export var move_duration: float = 0.12
+
+var is_moving: bool = false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	 # idle when not moving 
-	if animated_sprite.sprite_frames and animated_sprite.sprite_frames.has_animation("idle"):
-		animated_sprite.play("idle")
+	position = position.snapped(Vector2(tile_size, tile_size))
+	play_animation("idle")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _unhandled_input(event: InputEvent) -> void:
+	if is_moving:
+		return
+	if event.is_action_pressed("move_left"):
+		try_move(Vector2.LEFT)
+	elif event.is_action_pressed("move_right"):
+		try_move(Vector2.RIGHT)
+	elif event.is_action_pressed("move_up"):
+		try_move(Vector2.UP)
+	elif event.is_action_pressed("move_down"):
+		try_move(Vector2.DOWN)
+	
+	
+func try_move(direction: Vector2) -> void:
+	var target_pos: Vector2 = position + direction * tile_size
+	face_direction(direction)
+	
+	if not is_walkable(target_pos):
+		play_animation("push")
+		return
+		
+	is_moving = true
+	play_animation(direction_to_anim(direction))
+	
+	var tween := create_tween()
+	tween.tween_property(self, "position", target_pos, move_duration)
+	tween.finished.connect(_on_move_finsihed)
+	
+
+func _on_move_finsihed() -> void:
+	is_moving = false
+	play_animation("idle")
+
+func is_walkable(target_pos: Vector2) -> bool:
+	var cell := tilemap.local_to_map(tilemap.to_local(target_pos))
+	var tile_data := tilemap.get_cell_tile_data(cell)
+	if tile_data == null:
+		return false
+	return not tile_data.get_custom_data("is_wall")
+
+func face_direction(direction: Vector2) -> void:
+	if direction == Vector2.LEFT:
+		animated_sprite.flip_h = true
+	elif direction == Vector2.RIGHT:
+		animated_sprite.flip_h = false
+
+func direction_to_anim(direction: Vector2) -> String:
+	if direction == Vector2.LEFT or direction == Vector2.RIGHT:
+		return "strafe"
+	elif direction == Vector2.UP:
+		return "move_up"
+	else:
+		return "move_down"
+
+func play_animation(anim_name: String) -> void:
+	if animated_sprite.sprite_frames and animated_sprite.sprite_frames.has_animation(anim_name):
+		animated_sprite.play(anim_name)
